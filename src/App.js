58b8b58037408d9328079faaa190f3b94832743e
melkam <melkammekonnen899@gmail.com>
2020-12-22 17:02:51 +0300

    Showing up contacts in the app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import Contacts from './components/contacts';
 
 class App extends Component {
   state = {
-    contact: []
+    contacts: []
   }
   
   componentDidMount(){
@@ -17,13 +18,7 @@ class App extends Component {
 
   render() {
     return(
-      <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Steve Jobs</h5>
-              <h6 class="card-subtitle mb-2 text-muted">steve@apple.com</h6>
-              <p class="card-text">Stay Hungry, Stay Foolish</p>
-            </div>
-          </div>
+      <Contacts contacts = {this.state.contact} />
     );
   }
 }
